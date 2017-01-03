9c06e6ca3172f1474519b175e964c53b1e30f50e
Mario <mdiez12@gmail.com>
2017-01-03 16:17:14 +0100

    files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const async = require('async');
+
+app.get('/upcoming', (req, res) => {
+    async.times(2, (i, callback) => {
+    var options = {
+        url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=60635989a212412cfd15d67d0fa08f3d',
+        qs: {
+            'language': 'en-US',
+            'page': i+1,
+            'region': 'us'
+        },
+    } 
+    request(options, (error, response, body) => {
+        var result = JSON.parse(body)
+        var data = result.results;
+        callback(null, data);
+    });
+    }, (err, results) => {
+        res.json(results);
+    });
+})
+
+app.listen('8010', () => {
+    console.log('Listening on port 8010');
+})
